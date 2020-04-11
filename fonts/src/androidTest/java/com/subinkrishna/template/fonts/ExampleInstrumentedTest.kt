994395b28c024da7274495461705b6b803130ec8
Subinkrishna Gopi <subinkrishna.gopi@gmail.com>
2020-04-11 16:49:13 -0400

    chore: renamed package & updated gradle

--- fonts/src/androidTest/java/com/subinkrishna/template/fonts/ExampleInstrumentedTest.kt
+++ fonts/src/androidTest/java/com/subinkrishna/template/fonts/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.subinkrishna.splash.fonts.test", appContext.packageName)
+    assertEquals("com.subinkrishna.template.fonts.test", appContext.packageName)
   }
 }
